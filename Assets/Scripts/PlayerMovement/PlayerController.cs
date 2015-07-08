35098554c33e4b159e20df730fb123f1830c743e
MikeyUchiha <mikey@starfantasygames.com>
2015-07-07 23:02:24 -0700

    Created a PlayerController script and added Ground Textures
    Created a PlayerMovement script to handle basic movement. Created a
    PlayerController script to have a smoother movement of the character.
    Added ground textures to the project.

---
+++ Assets/Scripts/PlayerMovement/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float rotateSpeed = 5.0f;
+	public float forwardSpeed = 15.0f;
+	private CharacterController playerController;
+
+	// Use this for initialization
+	void Start () {
+		playerController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space") && playerController.isGrounded){
+			playerController.Move(Vector3.up);
+		}
+		transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		float speed = forwardSpeed * Input.GetAxis ("Vertical");
+		playerController.SimpleMove(speed * forward);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed = 10.0f;
+	public float sprintSpeed = 20.0f;
+	public float rotateSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("w") && Input.GetKey("left shift")){
+			transform.Translate((Vector3.forward) * sprintSpeed * Time.deltaTime);
+		}
+		else if(Input.GetKey("w")){
+			transform.Translate((Vector3.forward) * moveSpeed * Time.deltaTime);
+		}
+		if(Input.GetKey("s")){
+			transform.Translate((Vector3.back) * moveSpeed * Time.deltaTime);
+		}
+		if(Input.GetKey("a")){
+			transform.Rotate((Vector3.down) * rotateSpeed);
+			//transform.Translate((Vector3.left) * moveSpeed * Time.deltaTime);
+		}
+		if(Input.GetKey("d")){
+			transform.Rotate((Vector3.up) * rotateSpeed);
+			//transform.Translate((Vector3.right) * moveSpeed * Time.deltaTime);
+		}
+		// right mouse button
+		if(Input.GetMouseButton(1)){
+
+		}
+		// left mouse button
+		if(Input.GetMouseButton(0)){
+			
+		}
+	}
+}
