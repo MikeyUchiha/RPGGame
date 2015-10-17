15d01fcc750eb7489e56bca43cbe3b8340bbece8
MikeyUchiha <mikey@starfantasygames.com>
2015-10-16 21:41:48 -0700

    Updated Unity Version and added 2 new scripts
    Added CreateAPlayerGUI.cs which will create GUI for creating a player.
    Added DisplayCreatePlayerFunctions.cs to handle the GUI logic behind the
    CreateAPlayerGUI class.

---
+++ Assets/Scripts/CreateAPlayerGUI/CreateAPlayerGUI.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateAPlayerGUI : MonoBehaviour {
+
+	public enum CreateAPlayerStates{
+		CLASSSELECTION,	// Display all class types
+		STATALLOCATION,	// Allocate stats
+		FINALSETUP		// Add name and misc items
+	}
+
+	private DisplayCreatePlayerFunctions displayFunctions = new DisplayCreatePlayerFunctions();
+	public static CreateAPlayerStates currentState;
+
+	// Use this for initialization
+	void Start () {
+		currentState = CreateAPlayerStates.CLASSSELECTION;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(currentState){
+		case(CreateAPlayerStates.CLASSSELECTION):
+			break;
+		case(CreateAPlayerStates.STATALLOCATION):
+			break;
+		case(CreateAPlayerStates.FINALSETUP):
+			break;
+		}
+	}
+
+	void CreateUI(){
+
+	}
+
+	void ChangeUI(){
+		if(currentState == CreateAPlayerStates.CLASSSELECTION){
+			// Display class selection function
+			displayFunctions.DisplayClassSelection();
+		}
+		if(currentState == CreateAPlayerStates.STATALLOCATION){
+			// Display stat allocation function
+			displayFunctions.DisplayStatAllocation();
+		}
+		if(currentState == CreateAPlayerStates.FINALSETUP){
+			// Display final setup function
+			displayFunctions.DisplayFinalSetup();
+		}
+	}
+}
---
+++ Assets/Scripts/CreateAPlayerGUI/DisplayCreatePlayerFunctions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayCreatePlayerFunctions {
+
+	public void DisplayClassSelection(){
+		// A list of toggle buttons and each button will be a different class
+		// Selection Grid
+	}
+
+	public void DisplayStatAllocation(){
+		// A list of stats with plus and minus buttons to add stats
+		// Logic to make sure the player cannot add more than stats given
+	}
+
+	public void DisplayFinalSetup(){
+		// Name
+		// Gender
+		// Add a description to character
+	}
+}
