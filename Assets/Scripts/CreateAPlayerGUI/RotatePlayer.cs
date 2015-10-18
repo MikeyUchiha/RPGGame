87893e480caa67c350807c26f0445a40766ad279
MikeyUchiha <mikey@starfantasygames.com>
2015-10-17 18:13:20 -0700

    Added the ability to view a character model and rotate it
    Added the ability to view a character model and rotate it when creating
    a new character.

---
+++ Assets/Scripts/CreateAPlayerGUI/RotatePlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatePlayer : MonoBehaviour {
+
+	private Transform player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void RotatePlayerClick(){
+		if(this.name == "RotateLeft"){
+			player.Rotate(Vector3.up * 10);
+		}
+		if(this.name == "RotateRight"){
+			player.Rotate(Vector3.down * 10);
+		}
+	}
+}
